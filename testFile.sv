d553a1f5746066f378187140588f8020e052e6ee
Cheick Keita <chkeita@microsoft.com>
2016-05-09 21:41:30 -0700

    converting to SystemVerilog

--- testFile.sv
+++ testFile.sv
@@ -1,14 +1,34 @@
 //`timescale 1 ps / 1 ps
 // synopsys translate_on
-module I2S_Receiver (
-	input data,
-	output [31:0] left,
-	output [31:0] right
-	
-	
-	
-	);
+module deltaSigma_tb (
+    );
+    
+    reg clk;
+    reg [2:0] data = 3'b011;
+    reg reset;
+    wire dataOut;
+    
+// initial begin 
+//     assign clk     = 0;
+//     assign reset   = 0;
+//     assign dataOut = 0;
+// end
 
-	
+always begin
+    #10
+    clk <= ~clk;
+end
+    
+
+delta_sigma #(.DATA_SIZE(3)) dut  (
+    .data(data),
+    .clk(clkl),
+    .reset(reset),
+    .dataOut(dataOut)
+);
+    
+initial begin
+    $monitor("dataOut=%b",dataOut);
+end
 
 endmodule
